1effeb93d9b8dead2e6c14f5ecdd0d662b2d17de
Emerald <140569368+egolightly81@users.noreply.github.com>
2024-03-25 13:02:53 +0800

    Sytnax errors - Chat GPT assisted

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 /*
-Lets keep score! 
+Let's keep score! 
 */ 
 let wins = 0; 
-let loses =0; 
-let draws =0;
+let loses = 0; 
+let draws = 0;
 
-function say(themessage){
-    let msg = new SpeechSynthesisUtterance (themessage);
+function say(themessage) {
+    let msg = new SpeechSynthesisUtterance(themessage);
     speechSynthesis.speak(msg);
-
 }
 
 /* This is a simplified version of the game.
 The Computer ALWAYS picks ROCK.  
-Does the user's choose win,lose, or tie vs ROCK? */
+Does the user's choice win, lose, or tie vs ROCK? */
 function play(user) { 
     let result = "";
     if (user === 'rock') {
@@ -26,14 +25,12 @@ function play(user) {
     }
     if (user === 'scissors') {
         result = "loses";
-        loses++
+        loses++;
     }
-    let mymessage("Computer chose rock, you " + result + "!" + 
-    "\n\n" + wins + "wins" +
-    "\n\n" + loses + "loses" +
-    "\n\n" + draws + "draws" +
-    );
-    alert (mymessage)
-    say  (mymessage)
-
+    let mymessage = "Computer chose rock, you " + result + "!" + 
+        "\n\n" + wins + " wins" +
+        "\n\n" + loses + " loses" +
+        "\n\n" + draws + " draws";
+    alert(mymessage);
+    say(mymessage);
 }
