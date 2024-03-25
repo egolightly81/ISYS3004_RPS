3b07eb5f7cf7221e5dde5ffb6842e5ee3fe32575
Emerald <140569368+egolightly81@users.noreply.github.com>
2024-03-25 10:29:43 +0800

    adding value to the game

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+/*Lets keep score! */ 
+let win = 0; 
+let lose =0; 
+let draw =0;
+
+function say(themessage){
+    let msg = new SpeechSynthesisUtterance (themessage);
+    speechSynthesis.speak(msg);
+
+}
+
+
 /* This is a simplified version of the game.
 The Computer ALWAYS picks ROCK.  
 Does the user's choose win,lose, or tie vs ROCK? */
@@ -5,12 +17,22 @@ function play(user) {
     let result = "";
     if (user === 'rock') {
         result = "tie";
+        draws++;
     }
     if (user === 'paper') {
         result = "win";
+        wins++;
     }
     if (user === 'scissors') {
         result = "lose";
+        loses++
     }
-    alert("Computer chose rock, you " + result + "!");
+    let mymessage("Computer chose rock, you " + result + "!" + 
+    "\n\n" + wins + "wins" +
+    "\n\n" + loses + "loses" +
+    "\n\n" + draws + "draws" +
+    );
+    alert (mymessage)
+    say  (mymessage)
+
 }
